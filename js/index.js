5587f3c57f122971b4a9101d5a9735f0cca55b3b
Reinaldo Trindade <rewifetri@gmail.com>
2020-05-07 18:05:50 -0300

    test on map markers

--- js/index.js
+++ js/index.js
@@ -312,7 +312,7 @@ flatpickr("#datepicker", {
 
 let mymap = L.map('map', {
   scrollWheelZoom: false
-}).setView([51.505, -0.09], 13);
+}).setView({lat: 51.509334, lon: -0.114883}, 13);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoicmVpbmFsZG9vbyIsImEiOiJjazl2a3g1ZXAwMDlxM2dvNGJ5M3ZjZDJ3In0.1B7OE0PYPCDADiruf5hh7A', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
